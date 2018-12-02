c09093e28cc7617e284e4edacd0938d6bfd72899
Wilson Tang <wilson.t.tang@gmail.com>
2018-12-02 13:33:31 -0500

    big update w/ keras-retinanet

---
+++ local_validation.py
@@ -0,0 +1,111 @@
+# Function is based off of work done to get the IoU for the rsna kaggle competition: https://www.kaggle.com/chenyc15/mean-average-precision-metric 
+"""
+Automatically finds the accuracy of the labels and predictions files 
+outputs an array of 
+Outputs: accuracy 
+"""
+# import libraries
+import numpy as np 
+import os 
+import csv
+
+# directory
+label_file_path = './data/stage_2_labels.csv'
+predictions_file_path = './data/stage_2_predictions.csv'
+scores[]
+# load data 
+with open('testfile.csv', newline='') as csvfile:
+    data_labels = list(csv.reader(csvfile))
+with open('testfile.csv', newline='') as csvfile:
+    data_predictions = list(csv.reader(csvfile))
+
+# calculate accuracy 
+
+count = 0
+sum = 0.0
+
+for image in 
+map_iou(np.array(data_labels[1:4],np.array(data_predictions[1:4]), np.array(data_predictions[5]))
+
+for k in data_predictions.keys():
+    print(data_labels[k], data_predictions[k]['bbox'], data_predictions[k]['score'])
+    if data_labels[k] == []:
+        if pd[k]['bbox'] == []:
+            continue
+        else:
+            count += 1
+    else:
+        sum += map_iou(np.array(data_labels[k]), np.array(data_predictions[k]['bbox']), np.array(data_predictions[k]['score']))
+        count += 1
+        print(map_iou(np.array(data_labels[k]), np.array(data_predictions[k]['bbox']), np.array(data_predictions[k]['score'])))
+print(sum/count)
+
+# helper function to calculate IoU
+def iou(box1, box2):
+    x11, y11, w1, h1 = box1
+    x21, y21, w2, h2 = box2
+    assert w1 * h1 > 0
+    assert w2 * h2 > 0
+    x12, y12 = x11 + w1, y11 + h1
+    x22, y22 = x21 + w2, y21 + h2
+
+    area1, area2 = w1 * h1, w2 * h2
+    xi1, yi1, xi2, yi2 = max([x11, x21]), max([y11, y21]), min([x12, x22]), min([y12, y22])
+    
+    if xi2 <= xi1 or yi2 <= yi1:
+        return 0
+    else:
+        intersect = (xi2-xi1) * (yi2-yi1)
+        union = area1 + area2 - intersect
+        return intersect / union
+
+def map_iou(boxes_true, boxes_pred, scores, thresholds = [0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75]):
+    """
+    Mean average precision at differnet intersection over union (IoU) threshold
+    
+    input:
+        boxes_true: Mx4 numpy array of ground true bounding boxes of one image. 
+                    bbox format: (x1, y1, w, h)
+        boxes_pred: Nx4 numpy array of predicted bounding boxes of one image. 
+                    bbox format: (x1, y1, w, h)
+        scores:     length N numpy array of scores associated with predicted bboxes
+        thresholds: IoU shresholds to evaluate mean average precision on
+    output: 
+        map: mean average precision of the image
+    """
+    
+    # According to the introduction, images with no ground truth bboxes will not be 
+    # included in the map score unless there is a false positive detection (?)
+        
+    # return None if both are empty, don't count the image in final evaluation (?)
+    if len(boxes_true) == 0 and len(boxes_pred) == 0:
+        return None
+    
+    assert boxes_true.shape[1] == 4 or boxes_pred.shape[1] == 4, "boxes should be 2D arrays with shape[1]=4"
+    if len(boxes_pred):
+        assert len(scores) == len(boxes_pred), "boxes_pred and scores should be same length"
+        # sort boxes_pred by scores in decreasing order
+        boxes_pred = boxes_pred[np.argsort(scores)[::-1], :]
+    
+    map_total = 0
+    
+    # loop over thresholds
+    for t in thresholds:
+        matched_bt = set()
+        tp, fn = 0, 0
+        for i, bt in enumerate(boxes_true):
+            matched = False
+            for j, bp in enumerate(boxes_pred):
+                miou = iou(bt, bp)
+                if miou >= t and not matched and j not in matched_bt:
+                    matched = True
+                    tp += 1 # bt is matched for the first time, count as TP
+                    matched_bt.add(j)
+            if not matched:
+                fn += 1 # bt has no match, count as FN
+                
+        fp = len(boxes_pred) - len(matched_bt) # FP is the bp that not matched to any bt
+        m = tp / (tp + fn + fp)
+        map_total += m
+    
+    return map_total / len(thresholds)
